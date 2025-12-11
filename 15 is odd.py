16b01206a518d56d7330459cbedda240e3c4edac
pipwithit <pipewithit@gmail.com>
2025-12-11 18:45:09 -0500

    Add files via upload

---
+++ 15 is odd.py
@@ -0,0 +1,165 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]:
+    
+SyntaxError: invalid syntax
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+for number in numbers
+SyntaxError: expected ':'
+for number in numbers:
+    if (number % 2 == 0)
+    
+SyntaxError: expected ':'
+for number in numbers:
+    if (number % 2 == 0):
+        print("is even")
+    else:
+        print("is odd")
+
+        
+is odd
+is even
+is odd
+is even
+is odd
+is even
+is odd
+is even
+is odd
+is even
+is odd
+is even
+is odd
+is even
+is odd
+for number in numbers:
+    if (number % 2 == 0):
+        print(number "is even")
+    else:
+        print(number "is odd")
+        
+SyntaxError: invalid syntax
+numbint = "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"
+for number in numbers:
+    if (number % 2 == 0):
+        print(numbint "is even")
+    else:
+        print(numbint "is odd")
+        
+SyntaxError: invalid syntax
+for number in numbers:
+    if (number % 2 == 0):
+        print("is even")
+    else:
+        print("is odd")
+for number in numbers:
+    
+SyntaxError: invalid syntax
+
+
+
+
+for number in numbers:
+    print(number)
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+str(number)
+'15'
+str(numbers)
+'[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]'
+number
+15
+print(number)
+15\
+for number in numbers:
+    print(number)
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+str(number)
+'15'
+for number in numbers:
+    if (number % 2 == 0):
+        print(str(numbers) "is even")
+    else:
+        print(str(numbers) "is odd")
+        
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+odd = "is odd"
+even =
+SyntaxError: invalid syntax
+even =
+SyntaxError: invalid syntax
+even = "is even"
+for number in numbers:
+    if (number % 2 == 0):
+        print(str(numbers), even)
+    else:
+...         print(str(numbers), odd)
+... 
+...         
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is even
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is even
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is even
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is even
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is even
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is even
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is even
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] is odd
+>>> for number in numbers:
+...     if (number % 2 == 0):
+...         print(str(number), even)
+...     else:
+...         print(str(number), odd)
+... 
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
+6 is even
+7 is odd
+8 is even
+9 is odd
+10 is even
+11 is odd
+12 is even
+13 is odd
+14 is even
+15 is odd
